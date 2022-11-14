54b24b0bd04a120ac707b37643b3a883a7f49177
Robert Gomez <rtirado@altice.com.do>
2022-11-14 15:02:19 -0400

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,24 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'test-error-path',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    cleartext: true,
+    errorPath: 'assets/error.html'
+  },
+  /*
+    Test 1.
+    Run the Android App with the configuration below and you will see black screen.
+    Test 2.
+    Run the Android App without the configuration below and you will see the custom html file with error message.
+   */
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+      backgroundColor: '#000000',
+      androidScaleType: 'CENTER_CROP'
+    },
+  },
 };
 
 export default config;
